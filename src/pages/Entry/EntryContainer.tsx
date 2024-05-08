b5e8448da2f36cf0b6d6188dd78255af640acf38
이승연 <sylee1274@mz.co.kr>
2024-05-08 22:28:19 +0900

    Chore: design update

--- src/pages/Entry/EntryContainer.tsx
+++ src/pages/Entry/EntryContainer.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import EntryLogin from './_partials/EntryLogin';
 import EntryMenu from './_partials/EntryMenu';
 import { useSelector } from 'react-redux';
+import LogoHybrix from '@/assets/icons/LogoHybrix.svg';
 
 const EntryContainer = () => {
   const [loginStatus, setLoginStatus] = useState<boolean>(false);
@@ -10,9 +11,9 @@ const EntryContainer = () => {
   return (
     <div>
       <div className='absolute inset-0 bg-gradient-to-r from-[#6667AB] to-[#ED8C00]'>{/* <img src="/assets/images/auth/bg-gradient.png" alt="image" className="h-full w-full object-fill" /> */}</div>
-      <div className='absolute p-4 text-3xl font-bold font-mono'>
-        {/* logo */}
-        IAMPAM
+      <div className='absolute p-2 text-2xl font-bold flex gap-3'>
+        <img className='w-8 ltr:-ml-1 rtl:-mr-1 inline' src={LogoHybrix} alt='logo' />
+        <p>IAMPAM</p>
       </div>
 
       <div className='relative flex min-h-screen items-center justify-center  sm:px-16 pt-12'>{user.email && user.password ? <EntryMenu /> : <EntryLogin setLoginStatus={setLoginStatus} />}</div>
--- src/pages/Entry/_partials/EntryMenu.tsx
+++ src/pages/Entry/_partials/EntryMenu.tsx
@@ -1,18 +1,12 @@
 import IconLogout from '@/components/Icon/IconLogout';
 import { useNavigate } from 'react-router-dom';
-import IconUsers from '@/assets/icons/group.png';
 import IconTopology from '@/assets/icons/IconTopology.svg';
 import IconLock from '@/assets/icons/IconLock.svg';
 import IconLink from '@/assets/icons/IconLink.svg';
-import IconIAMMonitor from '@/assets/icons/IconIAMMonitor.svg';
-import IconIAMAudit from '@/assets/icons/IconIAMAudit.svg';
-import IconCompliance from '@/assets/icons/IconCompliance.svg';
 import IconCost from '@/assets/icons/IconCost.svg';
 import IconUser from '@/assets/icons/IconUser.svg';
 import IconResources from '@/assets/icons/IconResources.svg';
 import LogoCloudOps from '@/assets/icons/LogoCloudOps.svg';
-import a from '@/assets/icons/a.svg';
-import b from '@/assets/icons/b.svg';
 
 const EntryMenu = () => {
   const menuList = [
@@ -53,25 +47,36 @@ const EntryMenu = () => {
   const navigateMenu = (path: string) => navigate(path);
 
   return (
-    <div className='relative  panel flex flex-col gap-16 h-full w-full bg-[#F6F5F2] rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50 border border-gray-100 p-12 border-none'>
+    <div className='relative panel flex flex-col gap-4 w-[1000px] bg-[#F6F5F2] rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50 border border-gray-100 p-14 border-none'>
       <div className='flex gap-8 justify-center items-center relative'>
         <img className='w-16 p-2 bg-[#6667AB] rounded-xl' src={LogoCloudOps} alt='' />
-        <p className='text-6xl font-semibold font-mono tracking-tighter'>CloudOps Portal</p>
+        <p className='text-6xl font-semibold tracking-tighter'>CloudOps Portal</p>
       </div>
 
-      <div className='flex gap-16 p-8 items-center justify-center'>
-        {menuList.map((menu, idx) => (
-          <button className='lg:col-span-1 flex flex-col items-center rounded hover:text-[#FEFAF6] hover:cursor-pointer p-8' onClick={() => navigateMenu(menu['path'])}>
-            <img className='w-12' src={menu['logo']} alt={menu['menu']} />
-            <p className='font-extrabold text-xl tracking-tighter mt-8'>{menu['menu']}</p>
-          </button>
-        ))}
+      <div className='grid lg:grid-cols-3 gap-4 p-8 items-center'>
+        {menuList.map((menu, idx) => {
+          if (menu.menu === 'Topology' || menu.menu === 'Security Group') {
+            return (
+              <button className='lg:col-span-1 flex flex-col items-center rounded hover:text-[#6667AB] hover:cursor-pointer p-8 fill-[#000]' onClick={() => navigateMenu(menu['path'])}>
+                <img className='w-12 fill-[#000]' src={menu['logo']} alt={menu['menu']} />
+                <p className='font-extrabold text-xl tracking-tighter mt-8'>{menu['menu']}</p>
+              </button>
+            );
+          } else {
+            return (
+              <button className='lg:col-span-1 flex flex-col items-center rounded hover:cursor-not-allowed p-8 fill-[#000]' onClick={() => navigateMenu(menu['path'])}>
+                <img className='w-12 fill-[#000]' src={menu['logo']} alt={menu['menu']} />
+                <p className='font-extrabold text-xl tracking-tighter mt-8'>{menu['menu']}</p>
+              </button>
+            );
+          }
+        })}
       </div>
 
       <div className=''>
         <button className='btn bg-[#6667AB] border-none w-full'>
           <IconLogout className='text-[#fff]' />
-          <p className='font-bold font-mono text-[#FFF] tracking-tight text-lg ml-2 shadow-none'>LOGOUT</p>
+          <p className='font-bold text-[#FFF] tracking-tight text-lg ml-2 shadow-none'>LOGOUT</p>
         </button>
       </div>
     </div>
