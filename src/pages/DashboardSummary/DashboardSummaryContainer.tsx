31089e4b702a5d24f5fb6e7ef4621b69680c3f74
이승연 <sylee1274@mz.co.kr>
2024-06-11 14:20:13 +0900

    Update: Summary Background Update

--- src/pages/DashboardSummary/DashboardSummaryContainer.tsx
+++ src/pages/DashboardSummary/DashboardSummaryContainer.tsx
@@ -1,5 +1,3 @@
-import CloudOpsTop from '@/assets/images/CloudOps-top.svg';
-import CloudOpsBottom from '@/assets/images/CloudOps-bottom.svg';
 import elbSeoul from '@/assets/images/elb-seoul.svg';
 import regionalResourcesOregon from '@/assets/images/regional-resources-oregon.svg';
 import subnetRoutesSeoulA from '@/assets/images/subnet-routes-seoulA.svg';
@@ -12,6 +10,9 @@ import networkFlowImage2 from '@/assets/images/networkflow-image2.png';
 import networkFlowImage3 from '@/assets/images/networkflow-image3.png';
 import networkFlowImage4 from '@/assets/images/networkflow-image4.png';
 import { useState } from 'react';
+import DashboardBanner from '@/assets/images/dashboard_main.svg';
+import DashboardTop from '@/assets/images/dashboard_top.svg';
+import DashboardBottom from '@/assets/images/dashboard_bottom.svg';
 
 const images = [elbSeoul, regionalResourcesOregon, subnetRoutesSeoulA, vpcGatewaysSeoul, subnetSeoulA, subnetSeoulB, vpcGatewayVirginia, vpcGatewayVirginia, vpcGatewayVirginia, vpcGatewayVirginia];
 const networkFlowImages = [networkFlowImage1, networkFlowImage2, networkFlowImage3, networkFlowImage4, networkFlowImage2, networkFlowImage3];
@@ -30,8 +31,8 @@ const DashboardSummaryContainer = () => {
 
   return (
     <div className='w-[1550px]'>
-      <img src={CloudOpsTop} alt='' />
-
+      <img src={DashboardBanner} alt='' />
+      <img className='mt-[1.2rem]' src={DashboardTop} alt='' />
       <div className='relative w-full overflow-hidden mt-8 mb-8'>
         <div className='flex transition-transform duration-500 gap-[24px]' style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
           {images.map((image, index) => (
@@ -47,7 +48,7 @@ const DashboardSummaryContainer = () => {
           &#9654;
         </button>
       </div>
-      <img src={CloudOpsBottom} alt='' />
+      <img src={DashboardBottom} alt='' />
       <div className='relative w-full overflow-hidden mt-8 mb-8'>
         <div className='flex transition-transform duration-500 gap-[24px]' style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
           {networkFlowImages.map((image, index) => (
